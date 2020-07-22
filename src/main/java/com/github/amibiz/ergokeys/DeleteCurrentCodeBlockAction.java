fafb8ed9e283139abd43e7f927d17b7ea7a0cd70
amibiz <ami.bizamcher@gmail.com>
2020-07-22 23:05:21 +0300

    Support plugin actions while updating indices (#23)
    Fixes #17

--- src/main/java/com/github/amibiz/ergokeys/DeleteCurrentCodeBlockAction.java
+++ src/main/java/com/github/amibiz/ergokeys/DeleteCurrentCodeBlockAction.java
@@ -1,11 +1,11 @@
 package com.github.amibiz.ergokeys;
 
 import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.IdeActions;
+import com.intellij.openapi.project.DumbAwareAction;
 
-public class DeleteCurrentCodeBlockAction extends AnAction {
+public class DeleteCurrentCodeBlockAction extends DumbAwareAction {
     final private ActionManager actionManager = ActionManager.getInstance();
 
     @Override
